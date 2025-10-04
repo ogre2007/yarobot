e8c23daebf5fdceec3f43eea1dc1e8b27cc5b735
oogre2007 <oogre@vk.com>
2025-10-04 16:21:51 +0300

    wip

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import binascii
 import datetime
 import gzip
 import json
@@ -5,9 +6,158 @@ import os
 import re
 import sys
 import traceback
+import lief
 from lxml import etree
 
 PE_STRINGS_FILE = "./3rdparty/strings.xml"
+
+
+
+
+# TODO: Still buggy after port to Python3
+def extract_hex_strings(s):
+    strings = []
+    hex_strings = re.findall(b"([a-fA-F0-9]{10,})", s)
+    for string in list(hex_strings):
+        hex_strings += string.split(b"0000")
+        hex_strings += string.split(b"0d0a")
+        hex_strings += re.findall(
+            b"((?:0000|002[a-f0-9]|00[3-9a-f][0-9a-f]){6,})", string, re.IGNORECASE
+        )
+    hex_strings = list(set(hex_strings))
+    # ASCII Encoded Strings
+    for string in hex_strings:
+        for x in string.split(b"00"):
+            if len(x) > 10:
+                strings.append(x)
+    # WIDE Encoded Strings
+    for string in hex_strings:
+        try:
+            if len(string) % 2 != 0 or len(string) < 8:
+                continue
+            # Skip
+            if b"0000" in string:
+                continue
+            dec = string.replace(b"00", b"")
+            if is_ascii_string(dec, padding_allowed=False):
+                strings.append(string)
+        except Exception as e:
+            traceback.print_exc()
+    return strings
+
+
+def extract_strings(maxlen, fileData) -> list[str]:
+    # String list
+    cleaned_strings = []
+    # Read file data
+    try:
+        # Read strings
+        strings_full = re.findall(b"[\x1f-\x7e]{6,}", fileData)
+        strings_limited = re.findall(b"[\x1f-\x7e]{6,%d}" % maxlen, fileData)
+        strings_hex = extract_hex_strings(fileData)
+        strings = list(set(strings_full) | set(strings_limited) | set(strings_hex))
+        wide_strings = [ws for ws in re.findall(b"(?:[\x1f-\x7e][\x00]){6,}", fileData)]
+
+        # Post-process
+        # WIDE
+        for ws in wide_strings:
+            # Decode UTF16 and prepend a marker (facilitates handling)
+            wide_string = ("UTF16LE:%s" % ws.decode("utf-16")).encode("utf-8")
+            if wide_string not in strings:
+                strings.append(wide_string)
+        for string in strings:
+            # Escape strings
+            if len(string) > 0:
+                string = string.replace(b"\\", b"\\\\")
+                string = string.replace(b'"', b'\\"')
+            try:
+                if isinstance(string, str):
+                    cleaned_strings.append(string)
+                else:
+                    cleaned_strings.append(string.decode("utf-8"))
+            except AttributeError as e:
+                print(string)
+                traceback.print_exc()
+
+    except Exception as e:
+        print(string)
+        traceback.print_exc()
+        pass
+
+    return cleaned_strings
+
+
+def extract_opcodes(fileData) -> list[str]:
+    # Opcode list
+    opcodes = []
+
+    try:
+        # Read file data
+        binary = lief.parse(fileData)
+        ep = binary.entrypoint
+
+        # Locate .text section
+        text = None
+        if isinstance(binary, lief.PE.Binary):
+            for sec in binary.sections:
+                if (
+                    sec.virtual_address + binary.imagebase
+                    <= ep
+                    < sec.virtual_address + binary.imagebase + sec.virtual_size
+                ):
+                    print(f"EP is located at {sec.name} section")
+                    text = sec.content.tobytes()
+                    break
+        elif isinstance(binary, lief.ELF.Binary):
+            for sec in binary.sections:
+                if sec.virtual_address <= ep < sec.virtual_address + sec.size:
+                    print(f"EP is located at {sec.name} section")
+                    text = sec.content.tobytes()
+                    break
+
+        if text is not None:
+            # Split text into subs
+            text_parts = re.split(b"[\x00]{3,}", text)
+            # Now truncate and encode opcodes
+            for text_part in text_parts:
+                if text_part == "" or len(text_part) < 8:
+                    continue
+                opcodes.append(
+                    binascii.hexlify(text_part[:16]).decode(encoding="ascii")
+                )
+    except Exception as e:
+        traceback.print_exc()
+        pass
+
+    return opcodes
+
+
+def get_pe_info(fileData: bytes) -> tuple[str, list[str]]:
+    """
+    Get different PE attributes and hashes by lief
+    :param fileData:
+    :return:
+    """
+    imphash = ""
+    exports = []
+    # Check for MZ header (speed improvement)
+    if fileData[:2] != b"MZ":
+        return imphash, exports
+    try:
+
+        print("Extracting PE information")
+        binary: lief.PE.Binary = lief.parse(fileData)
+        # Imphash
+        imphash = lief.PE.get_imphash(binary, lief.PE.IMPHASH_MODE.PEFILE)
+        # Exports (names)
+        for exp in binary.get_export().entries:
+            exp: lief.PE.ExportEntry
+            exports.append(str(exp.name))
+    except Exception as e:
+        traceback.print_exc()
+        pass
+
+    return imphash, exports
 
 
 def get_abs_path(filename):
